f77bf1fa062f2ff6eb4db7d478634b5cdfbd377e
KShatZ <kirillshats@gmail.com>
2024-10-07 22:25:51 -0400

    Google recaptcha client-side

--- assets/scripts/contact-form.js
+++ assets/scripts/contact-form.js
@@ -16,38 +16,45 @@ contactForm.addEventListener("submit", async (e) => {
 
     const formData = new FormData(contactForm);
     const body = Object.fromEntries(formData.entries());
-
+    
     try {
-        const response = await fetch("/send-email", {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    
-        if (response.ok) {
-            contactForm.reset();
-            inquiryContainer.classList.add("status-active");
-            formStatus.classList.add("active");
-            formStatus.firstElementChild.innerText = "Your message has been sent successfully, we will be in touch with you soon!";
+        grecaptcha.ready(function () {
+            grecaptcha.execute("6Le2tVoqAAAAABwVor7zjLaav1mQh34i5Lny7evR", {action: "submit"}).then(async function (token) {
+                
+                body.captchaToken = token;
 
-        } else {
-            inquiryContainer.classList.add("status-active");
-            formStatus.classList.add("active", "error");
-    
-            switch(response.status) {
-                case 403: //Failed Captcha --TODO
-                    formStatus.firstElementChild.innerText = "Can't send your message at this time...";
-                    break;
-                case 500:
-                    formStatus.firstElementChild.innerText = "There was an issue sending your message. Please try again or send us an email at info@vihsolutions.com";
-                    break;
-                default:
-                    formStatus.firstElementChild.innerText = "An unexpected error occured...";
-                    break;
-            }
-        }
+                const response = await fetch("/send-email", {
+                    method: "POST",
+                    body: JSON.stringify(body),
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+
+                if (response.ok) {
+                    contactForm.reset();
+                    inquiryContainer.classList.add("status-active");
+                    formStatus.classList.add("active");
+                    formStatus.firstElementChild.innerText = "Your message has been sent successfully, we will be in touch with you soon!";
+        
+                } else {
+                    inquiryContainer.classList.add("status-active");
+                    formStatus.classList.add("active", "error");
+            
+                    switch(response.status) {
+                        case 403: //Failed Captcha --TODO
+                            formStatus.firstElementChild.innerText = "Can't send your message at this time...";
+                            break;
+                        case 500:
+                            formStatus.firstElementChild.innerText = "There was an issue sending your message. Please try again or send us an email at info@vihsolutions.com";
+                            break;
+                        default:
+                            formStatus.firstElementChild.innerText = "An unexpected error occured...";
+                            break;
+                    }
+                }
+            });
+        });
     } catch (error) {
         inquiryContainer.classList.add("status-active");
         formStatus.classList.add("active", "error");
