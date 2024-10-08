b76238f9ad148071fe88627d46c9a5e964b42e12
KShatZ <kirillshats@gmail.com>
2024-10-07 22:29:55 -0400

    Google recpatcha backend validation

--- flask-server/app.py
+++ flask-server/app.py
@@ -2,6 +2,7 @@ from os import getenv
 
 from flask import Flask, request, Response
 from flask_mail import Mail, Message
+import requests
 
 app = Flask(__name__)
 mail = Mail(app)
@@ -10,36 +11,52 @@ mail = Mail(app)
 @app.post("/send-email")
 def send_email():
 
-    # TODO: Captcha Stuff
-
     user_info = request.json
     
-    email = Message(
-        subject = "Website Contact Form Submission",
-        sender = getenv("VIH_EMAIL_FROM_ADDRESS"),
-        recipients = [getenv("VIH_EMAIL_TO_ADDRESS")],
-    )
-
-    email.html = f"""
-    <html>
-        <body>
-            <p></p>
-            <p><strong>Name: </strong>{user_info.get("name", "N/A")}</p>
-            <p><strong>Email: </strong>{user_info.get("email", "N/A")}</p>
-            <p><strong>Phone Number: </strong>{user_info.get("phone-number", "N/A")}</p>
-            <p><strong>Message: </strong>{user_info.get("inquiry", "N/A")}</p>
-        </body>
-    </html>
-    """
-
     try:
-        mail.send(email)
-        print("Email sent with info:", user_info)
+        r = requests.post(
+            "https://www.google.com/recaptcha/api/siteverify", 
+            data={
+                "secret": getenv("RECAPTCHA_SECRET"),
+                "response": user_info.get("captchaToken", ""),
+                "remoteip": request.headers.get("X-Real-IP", "")
+            }
+        )
+        captcha_response = r.json()
     except Exception as e:
-        print("There was an issue sending email:", e)
-        return Response(status=500)
+        print("Failed to validate recaptcha for request:", user_info)
 
-    return Response(status=200)
+    if captcha_response.get("success", False) and captcha_response.get("score", 0) >= 0.4:
+
+        email = Message(
+            subject = "Website Contact Form Submission",
+            sender = getenv("VIH_EMAIL_FROM_ADDRESS"),
+            recipients = [getenv("VIH_EMAIL_TO_ADDRESS")],
+        )
+
+        email.html = f"""
+        <html>
+            <body>
+                <p></p>
+                <p><strong>Name: </strong>{user_info.get("name", "N/A")}</p>
+                <p><strong>Email: </strong>{user_info.get("email", "N/A")}</p>
+                <p><strong>Phone Number: </strong>{user_info.get("phone-number", "N/A")}</p>
+                <p><strong>Message: </strong>{user_info.get("inquiry", "N/A")}</p>
+            </body>
+        </html>
+        """
+
+        try:
+            mail.send(email)
+            print("Email sent with info:", user_info)
+        except Exception as e:
+            print("There was an issue sending email:", e)
+            return Response(status=500)
+
+        return Response(status=200)
+    else:
+        print(f"Captcha failed! --- Request Body: {user_info} --- Captcha Body: {captcha_response}")
+        return Response(status=403)
 
 
 if __name__ == "__main__":
